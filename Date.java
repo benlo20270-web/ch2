ee22c493cc36f0ca5846be01caecb30b2eba4aa5
benlo20270-web <benlo20270@gmail.com>
2026-09-14 13:26:26 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,28 @@
+public class Date {
+	
+	public static void main(String[] args) {
+	String day= "Tuesday";
+	int date= 14;
+	String month= "december";
+	int year= 2026;
+	System.out.print("American format; ");
+	System.out.print(day);
+	System.out.print(", ");
+	System.out.print(month);
+	System.out.print(" ");
+	System.out.print(day);
+	System.out.print(", ");
+	System.out.print(year);
+	
+	System.out.print("European format: ");
+	System.out.print(day);
+	System.out.print(" ");
+	System.out.print(date);
+	System.out.print(" ");
+	System.out.print(month);
+	System.out.print(' ');
+	System.out.println(year);
+	
+	
+		}
+	}
